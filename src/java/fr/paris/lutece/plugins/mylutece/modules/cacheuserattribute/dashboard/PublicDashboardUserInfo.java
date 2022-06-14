7d63be5a3ce2f437f22876379aedf70a887a071f
nlegarrec <norbert.legarrec2@paris.fr>
2022-06-14 14:38:13 +0200

    remove getDashboardData, add getDashboardTemplate, getDashboardModel

--- src/java/fr/paris/lutece/plugins/mylutece/modules/cacheuserattribute/dashboard/PublicDashboardUserInfo.java
+++ src/java/fr/paris/lutece/plugins/mylutece/modules/cacheuserattribute/dashboard/PublicDashboardUserInfo.java
@@ -34,47 +34,51 @@
 package fr.paris.lutece.plugins.mylutece.modules.cacheuserattribute.dashboard;
 
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
 import fr.paris.lutece.plugins.mylutece.modules.cacheuserattribute.service.CacheUserAttributeService;
 import fr.paris.lutece.portal.service.dashboard.IPublicDashboardComponent;
 import fr.paris.lutece.portal.service.i18n.I18nService;
-import fr.paris.lutece.portal.service.template.AppTemplateService;
-import fr.paris.lutece.util.html.HtmlTemplate;
 
 /**
- * The Class MyPublicUserInfoProfile.
+ * The Class PublicDashboardUserInfo.
  */
 public class PublicDashboardUserInfo implements IPublicDashboardComponent
 {
 	public static final String DASHBOARD_PROPERTIES_TITLE = "module.mylutece.cacheuserattribute.publicdashboard.bean.title";
 	private String strIdComponent = "mylutece-cacheuserattribute.PublicDashboardUserInfo";
-	private static final String TEMPLATE_USER_INFORMATIONS = "/skin/plugins/publicdashboard/view_user_informations.html";
-	private static final String MARK_DASHBOARD_USER = "dashboardUserInformations";
+	private static final String TEMPLATE_USER_INFORMATIONS = "/skin/plugins/mylutece/modules/cacheuserattribute/publicdashboard_user_informations.html";
+	private static final String MARK_DASHBOARD_USER = "userInformations_publicdashboard";
 
 	@Override
-	public String getDashboardData( String user_id, Map<String,String> additionalParameters ) {
-		Map<String, Object> model = new HashMap<String, Object>( );
-		model.put( MARK_DASHBOARD_USER, searchUserInformations( user_id ) );
-        HtmlTemplate template = AppTemplateService.getTemplate( TEMPLATE_USER_INFORMATIONS, I18nService.getDefaultLocale( ), model );
-
-        return template.getHtml( );
-	}
-
-	@Override
-	public String getComponentDescription( ) {
-		return I18nService.getLocalizedString( DASHBOARD_PROPERTIES_TITLE, I18nService.getDefaultLocale( ) );
+	public String getComponentDescription( Locale locale ) {
+		return I18nService.getLocalizedString( DASHBOARD_PROPERTIES_TITLE, locale );
 	}
 
 	@Override
 	public String getComponentId( ) {
 		return strIdComponent;
 	}
+	
+	@Override
+	public String getDashboardTemplate( )
+	{
+		return TEMPLATE_USER_INFORMATIONS;
+	}
+	
+	@Override
+	public Map<String, Object> getDashboardModel( String user_id, Map<String,String[]> additionalParameters )
+	{
+		Map<String, Object> model = new HashMap<String, Object>( );
+		model.put( MARK_DASHBOARD_USER, searchUserInformations( user_id ) );
+		return model;
+	}
 
     /**
      * Search name.
      *
-     * @param key the key
+     * @param user_id the user_id
      * @return the map
      */
     private static Map<String, String> searchUserInformations( String user_id )
