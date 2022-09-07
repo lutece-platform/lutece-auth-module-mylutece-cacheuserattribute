b00499323473d8db91310359fb56aea2022a37ed
seboo <25958061+seboo@users.noreply.github.com>
2022-09-07 10:42:17 +0200

    CACHEUSRAT-1: Change user id and value field size

--- src/sql/plugins/mylutece/modules/cacheuserattribute/plugin/create_db_mylutece-cacheuserattribute.sql
+++ src/sql/plugins/mylutece/modules/cacheuserattribute/plugin/create_db_mylutece-cacheuserattribute.sql
@@ -6,9 +6,9 @@
 DROP TABLE IF EXISTS mylutece_cacheuserattribute_attribute;
 CREATE TABLE mylutece_cacheuserattribute_attribute (
 id_cache_user_attribute int AUTO_INCREMENT,
-id_user varchar(50) default '' NOT NULL,
+id_user varchar(56) default '' NOT NULL,
 id_attribute int default '0' NOT NULL,
-content varchar(50) default '',
+content long varchar default '',
 create_date date ,
 PRIMARY KEY (id_cache_user_attribute)
 );
