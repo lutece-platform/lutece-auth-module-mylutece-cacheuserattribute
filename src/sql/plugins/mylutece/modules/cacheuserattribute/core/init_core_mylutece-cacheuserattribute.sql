f8b575db19a0e962894e6bc2170cea5c59a01df9
atos-mohamed-b <126660650+atos-mohamed-b@users.noreply.github.com>
2025-02-26 17:25:02 +0100

    LUT-29517: Fixed the admin feature's path values in the database

--- src/sql/plugins/mylutece/modules/cacheuserattribute/core/init_core_mylutece-cacheuserattribute.sql
+++ src/sql/plugins/mylutece/modules/cacheuserattribute/core/init_core_mylutece-cacheuserattribute.sql
@@ -4,7 +4,7 @@
 --
 DELETE FROM core_admin_right WHERE id_right = 'MYLUTECE_CACHEUSERATTRIBUTE_MANAGEMENT';
 INSERT INTO core_admin_right (id_right,name,level_right,admin_url,description,is_updatable,plugin_name,id_feature_group,icon_url,documentation_url, id_order ) VALUES 
-('MYLUTECE_CACHEUSERATTRIBUTE_MANAGEMENT','mylutece-cacheuserattribute.adminFeature.ManageCacheUserAttribute.name',1,'jsp/admin/plugins/mylutece-cacheuserattribute/ManageCacheUserAttributes.jsp','mylutece-cacheuserattribute.adminFeature.ManageCacheUserAttribute.description',0,'mylutece-cacheuserattribute',NULL,NULL,NULL,4);
+('MYLUTECE_CACHEUSERATTRIBUTE_MANAGEMENT','module.mylutece.cacheuserattribute.adminFeature.ManageCacheUserAttribute.name',1,'jsp/admin/plugins/mylutece/modules/cacheuserattribute/ManageCacheUserAttributes.jsp','module.mylutece.cacheuserattribute.adminFeature.ManageCacheUserAttribute.description',0,'mylutece-cacheuserattribute',NULL,NULL,NULL,4);
 
 
 --
